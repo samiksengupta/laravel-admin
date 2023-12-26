0716a1f7a969465bf38dd103f576fe0945caf0b6
Samik Sengupta <samiksengupta@hotmail.com>
2023-12-26 18:07:11 +0530

    added a group property for form elements that can be used to disable form groups

--- resources/views/partials/form-gen.blade.php
+++ resources/views/partials/form-gen.blade.php
@@ -1,5 +1,9 @@
 @foreach($form['elements'] as $key => $element)
+    @if (@$element['group'] || (isset($element['type']) && $element['type'] !== 'hidden'))
     <div id="group_{{ $key }}" class="form-group">
-    @include('laravel-admin::partials.form-control-gen', ['key' => $key])
+        @include('laravel-admin::partials.form-control-gen', ['key' => $key])
     </div>
+    @else
+    @include('laravel-admin::partials.form-control-gen', ['key' => $key])
+    @endif
 @endforeach
