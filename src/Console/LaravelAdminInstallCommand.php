c9e23e4f2c2c8549f871ef33a9c800fd2d995a60
Samik Sengupta <samiksengupta@hotmail.com>
2023-12-27 18:13:12 +0530

    installer now publishes auth configs, allows publishing footer and writes fillables to child User Model if possible

--- src/Console/LaravelAdminInstallCommand.php
+++ src/Console/LaravelAdminInstallCommand.php
@@ -3,7 +3,10 @@
 namespace Samik\LaravelAdmin\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 
+use ReflectionClass;
+use LaravelAdmin;
 use Samik\LaravelAdmin\Database\Seeders\DatabaseSeeder;
 
 class LaravelAdminInstallCommand extends Command
@@ -33,15 +36,24 @@ class LaravelAdminInstallCommand extends Command
             // publish configuration
             $this->info("Publishing configuration...");
 
+            if(!$update || $force) $this->call('vendor:publish', ['--tag' => 'config', '--force' => true, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
+
             $replaceConfig = ($update && !$force) ? $this->confirm('Do you want to overwrite any existing LaravelAdmin config files in your project?', false) : true;
-            if($replaceConfig) $this->call('vendor:publish', ['--tag' => 'config', '--force' => $replaceConfig, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
+            if($replaceConfig) $this->call('vendor:publish', ['--tag' => 'admin-config', '--force' => $replaceConfig, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
             
             // publish assets
             $this->info("Publishing assets...");
             $this->call('vendor:publish', ['--tag' => 'primary-assets', '--force' => $force || $update, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
-
+            
             $replaceAssets = ($update && !$force) ? $this->confirm('Do you want to overwrite any existing LaravelAdmin asset files in your project?', false) : true;
             if($replaceAssets) $this->call('vendor:publish', ['--tag' => 'secondary-assets', '--force' => $replaceAssets, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
+            
+            // publish views
+            if(!file_exists(resource_path('views/vendor/laravel-admin/partials/footer.blade.php')) && $this->confirm('Do you want to override the footer in your project?', true)) {
+                $this->info("Publishing views...");
+                $this->call('vendor:publish', ['--tag' => 'footer', '--force' => false, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
+            }
+            else $this->info('Package\'s Footer was not published. You will need to override the footer manually at views/vendor/laravel-admin/partials/footer.blade.php.');
             
             // run migrations
             $this->info("Running migrations...");
@@ -56,7 +68,9 @@ class LaravelAdminInstallCommand extends Command
             $replaceData = ($update && !$force) ? $this->confirm('Do you want to overwrite any existing LaravelAdmin data files in your project?', false) : true;
             $this->call('vendor:publish', ['--tag' => 'data', '--force' => $replaceData, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
 
-            if(!$update && $this->confirm('Do you want to overwrite DatabaseSeeder in your Project from LaravelAdmin? (Recommended if installing in a fresh project)', (config('app.env') != 'production'))) $this->call('vendor:publish', ['--tag' => 'database-seeder', '--force' => true, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
+            if(!$update && $this->confirm('Do you want to overwrite DatabaseSeeder in your Project from LaravelAdmin? (Recommended if installing in a fresh project)', (config('app.env') != 'production'))) {
+                $this->call('vendor:publish', ['--tag' => 'database-seeder', '--force' => true, '--provider' => 'Samik\\LaravelAdmin\\LaravelAdminServiceProvider']);
+            }
             else $this->info('Project\'s DatabaseSeeder was not changed. You will need to include seeders manually in your Project\'s DatabaseSeeder.');
             
             // if full installation is opted
@@ -102,17 +116,51 @@ class LaravelAdminInstallCommand extends Command
 
             if(!$update) {
                 // try to make LaravelAdmin User model parent of the project's User model, if present
+                // and then make adjustments to support fields from the parent model
                 $this->info('Attempting to set LaravelAdmin User model as parent to your project\'s User model...');
                 if (file_exists(app_path('User.php')) || file_exists(app_path('Models/User.php'))) {
-                    $userPath = file_exists(app_path('User.php')) ? app_path('User.php') : app_path('Models/User.php');
-                    $str = file_get_contents($userPath);
+                    $modelPath = file_exists(app_path('User.php')) ? app_path('User.php') : app_path('Models/User.php');
+                    $modelContent = file_get_contents($modelPath);
+                    $modelPathInfo = pathinfo($modelPath);
+                    $modelClassNamespace = str($modelPathInfo['dirname'])
+                                            ->after('app' . DIRECTORY_SEPARATOR)
+                                            ->replace(DIRECTORY_SEPARATOR, '\\')
+                                            ->prepend('App\\')
+                                            ->append('\\')
+                                            ->append($modelPathInfo['filename'])
+                                            ->toString();
 
-                    if ($str !== false) {
-                        $str = str_replace('extends Authenticatable', "extends \Samik\LaravelAdmin\Models\User", $str);
-                        file_put_contents($userPath, $str);
+                    if ($modelContent !== false) {
+                        // Make project model extend the package model
+                        $modelContent = str_replace('extends Authenticatable', "extends \Samik\LaravelAdmin\Models\User", $modelContent);
+                        
+                        // Modify fillable property in child with parent if possible
+                        $parentClass = LaravelAdmin::modelClass('User');
+                        $parentReflection = new ReflectionClass($parentClass);
+                        $parentFillables = $parentReflection->hasProperty('fillable') ? collect($parentReflection->getProperty('fillable')->getValue(new $parentClass())) : collect([]);
+                        
+                        $childClass = get_class(app($modelClassNamespace));
+                        $childReflection = new ReflectionClass($childClass);
+                        $childFillables = $childReflection->hasProperty('fillable') ? collect($childReflection->getProperty('fillable')->getValue(new $childClass())) : collect([]);
+
+                        if($parentFillables->count()) {
+                            $modelContent = str_replace(
+                                "protected \$fillable = [",
+                                "protected \$fillable = [" . PHP_EOL . "\t\t" . implode(
+                                            ',' . PHP_EOL . "\t\t", 
+                                            $parentFillables
+                                            ->reject(fn($f) => $childFillables->contains($f))
+                                            ->map(fn($f) => "'{$f}'")
+                                            ->toArray()
+                                ) . ",",
+                                $modelContent
+                            );
+                        }
+
+                        file_put_contents($modelPath, $modelContent);
                         
                         // create extended Crud policy for project's User model
-                        $this->info("{$userPath} updated Successfully! Generating policy for User...");
+                        $this->info("{$modelPath} updated Successfully! Generating policy for User...");
                         $this->call('make:xpolicy', ['name' => "UserPolicy"]);
                     }
                 } 
@@ -122,8 +170,9 @@ class LaravelAdminInstallCommand extends Command
                 }
             }
 
-            $this->call('cache:clear');
-            $this->call('optimize');
+            $this->call('jwt:secret');
+
+            $this->call('optimize:clear');
 
             $this->info("Installed LaravelAdmin");
 
