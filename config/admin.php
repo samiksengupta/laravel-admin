9c4ecbe3083775c983ba7453fa0502c7a8cb5c34
Samik Sengupta <samiksengupta@hotmail.com>
2023-12-28 21:48:35 +0530

    reordered config items

--- config/admin.php
+++ config/admin.php
@@ -5,11 +5,15 @@
  */
 return [
     'auto_routing' => env('AUTO_ROUTING', true),
-    'app_asset_url' => env('ASSET_URL', config('app.url')),
-    'api_prefix' => env('API_PREFIX', 'api'),
     'admin_prefix' => env('ADMIN_PREFIX'),
+    'api_prefix' => env('API_PREFIX', 'api'),
+    'app_asset_url' => env('ASSET_URL', config('app.url')),
     'web_middlewares' => ['web', 'throttle:120,1'],
     'api_middlewares' => ['api', 'throttle:60,1'],
+    'web_admin_middlewares' => [
+        'web', 
+        'throttle:120,1'
+    ],
     'api_admin_middlewares' => [
         'throttle:api', 
         'throttle:240,1', 
@@ -20,7 +24,6 @@ return [
         \Illuminate\Routing\Middleware\SubstituteBindings::class
     ],
     'api_controller_namespace' => 'Samik\\LaravelAdmin\\Http\\Controllers\\Api',
-    'main_controller_namespace' => 'Samik\\LaravelAdmin\\Http\\Controllers\\Main',
     'admin_controller_namespace' => 'Samik\\LaravelAdmin\\Http\\Controllers\\Admin',
     'model_namespace' => 'Samik\\LaravelAdmin\\Models',
 ];
