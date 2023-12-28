6e9b9c3e5322ad074619e744ecc429ead36d3fbd
Samik Sengupta <samiksengupta@hotmail.com>
2023-12-28 16:44:08 +0530

    updated default footer text

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
         <b>App Version</b> {{ env('APP_VER', '1.0.0') }}
     </div>
-    <strong>Laravel Admin ({{ LaravelAdmin::getVersion() }}) by <a href="https://github.com/samiksengupta/">Samik Sengupta</a></strong>
+    <strong>Powered by <a target="_blank" href="https://github.com/samiksengupta/laravel-admin">Laravel Admin</a>&nbsp;({{ LaravelAdmin::getVersion() }})</strong>
 </footer>
